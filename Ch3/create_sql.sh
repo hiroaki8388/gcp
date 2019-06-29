73185a95dd61357c4c2d5b7f003bbd7ae0ba377b
Hiroaki Hasegawa <hiroaki@fringe81.com>
2019-06-29 20:24:02 +0900

    scriptを整理

--- Ch3/create_sql.sh
+++ Ch3/create_sql.sh
@@ -1,5 +1,5 @@
 # /bin/bash
-INSTANCE_NAME='flight'
+INSTANCE_NAME='flights'
 LOCAL_IP=$(curl http://ipecho.net/plain)
 MYSQL_IP=$(gcloud sql instances describe $INSTANCE_NAME --format="value(ipAddresses.ipAddress)") 
 # instance生成
@@ -8,6 +8,9 @@ MYSQL_IP=$(gcloud sql instances describe $INSTANCE_NAME --format="value(ipAddres
 # 使用している端末のipの権限を付与
 # 原著だとwgetコマンドを使用しているが、mac環境のためcurlを使用
  gcloud sql instances patch $INSTANCE_NAME --authorized-networks $LOCAL_IP
+
+# tableを定義
+mysql --host=$MYSQL_IP --user root --verbose < create_table.sql
 
 # data dirに入っているcsvをimport
 mysqlimport --local --host $MYSQL_IP --user root --ignore-lines=1 --fields-terminated-by=',' bts data/flights.csv-*
